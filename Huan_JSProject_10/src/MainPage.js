8e711777aba205a49f3455e714dbcf2c37822958
Farmer-Chandler <Chandler4770@gmail.com>
2016-12-31 22:52:48 +0900

    [Farmer-Chandler] #1 export.bat

--- Huan_JSProject_10/src/MainPage.js
+++ Huan_JSProject_10/src/MainPage.js
@@ -1,3 +1,8 @@
 function startHuan() {
   
+  // Main startHuan
+  $("#HuanJSProject").css("width", "500px");
+  $("#HuanJSProject").css("height", "500px");
+  $("#HuanJSProject").css("background-color", "rgb(255,125,0)");
+  
 };
