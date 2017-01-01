84ecf34f6d17383ad83213aa3f81f882de8b3e7a
Farmer-Chandler <Chandler4770@gmail.com>
2017-01-01 19:23:07 +0900

    [Farmer-Chandler] #3 export.bat

--- Huan_JSProject_10/bin/export.bat
+++ Huan_JSProject_10/bin/export.bat
@@ -1,6 +1,7 @@
 cls
 @echo off
 
+rem # path.prop > EXPORT_PATH / include folder name
 
 echo.
 echo. "# Export Start"
@@ -12,17 +13,13 @@ set _path=%_path:EXPORT_PATH=%
 set _path=%_path:~1%
 echo %_path%
 echo.
-rem xcopy "../*" , "%_path%" /E /R /C /Y /M> "%FILEXCOPY_LOG%"
-
 rem if exist %_path%/Huan_JSProject_10 (
 rem ) else (
 rem  mkdir %_path%/Huan_JSProject_10
 rem )
 xcopy ".." "%_path%" /e /c /y /i
-
 echo.
 echo.
 echo.
-
 
 pause
